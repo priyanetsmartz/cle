4876ad7df6e5ce624bdcd20b30897190180fd31e
Vishal Rana <vishal.rana@netsmartz.local>
2021-07-06 16:34:19 +0530

    working on facebook, google login and facebook twitter share

--- src/containers/Page/signup.tsx
+++ src/containers/Page/signup.tsx
@@ -1,7 +1,65 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
+import { FacebookShareButton, TwitterShareButton } from "react-share";
+import { Button } from 'antd';
+
 function RegistrationForm(props) {
+
+    const responseGoogle = (response) => {
+        console.log(response);
+    }
+
+    const responseFacebook = (response) => {
+        console.log(response);
+    }
+
+    const componentClicked = (response) => {
+        console.log(response);
+    }
+
+    const signup = (res) => {
+        console.log(res);
+        const googleresponse = {
+            Name: res.profileObj.name,
+            email: res.profileObj.email,
+            token: res.googleId,
+            Image: res.profileObj.imageUrl,
+            ProviderId: 'Google'
+        };
+    }
+
     return (
         <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+            <GoogleLogin
+                clientId="788786912619-k4tb19vgofvmn97q1vsti1u8fnf8j6pa.apps.googleusercontent.com"
+                buttonText="Login with Google"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle} ></GoogleLogin>
+            <br />
+            <FacebookLogin
+                appId="1088597931155576"
+                autoLoad={true}
+                fields="name,email,picture"
+                onClick={componentClicked}
+                callback={responseFacebook} />
+            <br />
+            <br />
+            <p>adasd</p>
+            <FacebookShareButton
+                url={'https://www.npmjs.com/package/react-share'}
+                quote={'Sample text to share'}>
+                <Button>Facebook</Button>
+            </FacebookShareButton>
+
+            <TwitterShareButton
+                url={'https://www.npmjs.com/package/react-share'}
+                title={'Sample text to share'}>
+                <Button>Twitter</Button>
+            </TwitterShareButton>
+
+            <br />
+            <br />
             <form>
                 <div className="form-group text-left">
                     <label htmlFor="exampleInputEmail1">Email address</label>
