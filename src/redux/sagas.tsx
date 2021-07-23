3832974749601eb14195869ac8b8a395171282a8
Vishal Rana <vishal.rana@netsmartz.local>
2021-07-23 10:37:40 +0530

    working on forgot password

--- src/redux/sagas.tsx
+++ src/redux/sagas.tsx
@@ -1,8 +1,7 @@
 import { all } from "redux-saga/effects";
 import authSagas from "./auth/saga";
-import forgotPassword from "./auth/forgot-password.saga";
 import postCommentRequest from "./auth/post-comment.saga";
 
 export default function* rootSaga(getState) {
-    yield all([authSagas(), forgotPassword(), postCommentRequest()]);
+    yield all([authSagas(), postCommentRequest()]);
 }
