382e721e4c3dd7f2ab21621195add909ad2dc678
Akhil Mittal <akhil.mittal@netsmartz.net>
2021-12-15 18:14:12 +0530

    Login pop up code pushed

--- src/containers/Page/business/dashboard.tsx
+++ src/containers/Page/business/dashboard.tsx
@@ -1,64 +1,39 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
+
+import Modal from "react-bootstrap/Modal";
+
+import { Link } from "react-router-dom";
+import HtmlContent from '../../partials/htmlContent';
 
 function Dashboard(props) {
+    const [myDashboardModal, setMyDashboardModal] = useState(true);
     useEffect(() => {
     }, [])
 
+    const openDashboardModal = () => {
+        setMyDashboardModal(!myDashboardModal);
+    }
+
     return (
-        <div className="col-sm-9">
-            <section className="my_profile_sect mb-4">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <h1>Important announcements</h1>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-9">
-                            <div id="page-wrapper" className="container">
+        <Modal show={myDashboardModal} >
+                <div className="CLE_pf_details">
+                    <Modal.Header>
+                        <h1>Dashboard Details</h1>
+                        <Link to="#" onClick={openDashboardModal} className="cross_icn"> <i className="fas fa-times"></i></Link>
+                    </Modal.Header>
+                    <Modal.Body className="arabic-rtl-direction">
+                    <div className="section" >
+                <HtmlContent identifier="login_popup" />
+            </div>
+                    </Modal.Body>
+                    
 
-                                <div className="row">
-                                    <div className="col-lg-6">
-                                        <div className="panel panel-info">
-                                            <div className="panel-heading">
-                                                <div className="row">
-                                                    <div className="col-xs-6">
-                                                        <i className="fa fa-users fa-5x"></i>
-                                                    </div>
-                                                    <div className="col-xs-6 text-right">
-                                                        <p className="announcement-heading">1</p>
-                                                        <p className="announcement-text">Users</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6">
-                                        <div className="panel panel-warning">
-                                            <div className="panel-heading">
-                                                <div className="row">
-                                                    <div className="col-xs-6">
-                                                        <i className="fa fa-check fa-5x"></i>
-                                                    </div>
-                                                    <div className="col-xs-6 text-right">
-                                                        <p className="announcement-heading">12</p>
-                                                        <p className="announcement-text">To-Do Items</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
                 </div>
-            </section>
-        </div>
+            </Modal>
+       
     )
 }
 const mapStateToProps = (state) => {
